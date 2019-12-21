44a6f5cf020f284603222816d3df9c6f3ad295c4
Ruthvik Penubarthi <ruthvik.penubarthi@gmail.com>
2019-12-20 21:24:47 -0500

    Initial Commit

---
+++ Attendist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Attendist
+//
+//  Created by Ruthvik Penubarthi on 12/12/19.
+//  Copyright © 2019 Ruthvik Penubarthi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    var x = 5
+}
+
